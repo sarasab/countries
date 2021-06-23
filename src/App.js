7d67d073023b8b8c3d4caf8c8028e3336c8ba6ef
sara <sara.sabinm@gmail.com>
2021-06-23 09:36:05 +0200

    hooks

--- src/App.js
+++ src/App.js
@@ -1,55 +1,44 @@
-import React from 'react'
+import React, {useState, useEffect } from 'react'
 import Button from './component/Button'
 import Card from './component/Card'
 
-class App extends React.Component {
-  constructor() {
-    super()
-    this.state = {
-      name: '',
-      capital: '',
-      flag: '',
-      population: '',
-      region: '',
-    }
-  }
+const App = () => {
+  const [contryName, setCountryName] = useState('france')
+  const [name, setName] = useState('name')
+  const [capital, setCapital] = useState('capital')
+  const [flag, setFlag] = useState('flag')
+  const [population, setPopulation] = useState('population')
+  const [region, setRegion] = useState('region')
 
-
-
-  
-  getCountry = async (country) => {
+  useEffect(async () => {
     try {
-      const result = await fetch('https://restcountries.eu/rest/v2/name/' + country)
-      
+      const result = await fetch('https://restcountries.eu/rest/v2/name/' + contryName)
       const countries = await result.json()
       
-      this.setState({
-        name: countries[0].name,
-        capital: countries[0].capital,
-        flag: countries[0].flag,
-        population: countries[0].population,
-        region: countries[0].region,
-      })
+      setName(countries[0].name)
+      setCapital(countries[0].capital)
+      setFlag(countries[0].flag)
+      setPopulation(countries[0].population)
+      setRegion(countries[0].region)
+
     } catch (error) {
       console.error(error)
     }
-  }
+  })
 
-  render() {
-    return <div>
-     <div className="text-center">
-        <Button onClick={() => this.getCountry('France')}>France</Button>
-        <Button onClick={() => this.getCountry('Brazil')}>Brazil</Button>
-        <Button onClick={() => this.getCountry('Croatia')}>Croatia</Button>
-        <Button onClick={() => this.getCountry('Morocco')}>Morocco</Button>
-      </div>
-      <Card {...this.state}/>
+  return (
+    <div>
+      <div className="text-center" >
+        <Button onClick={() => setCountryName('France')}> France </Button >
+        <Button onClick={() => setCountryName('Brazil')}> Brazil</Button >
+        <Button onClick={() => setCountryName('Croatia')}> Croatia</Button >
+        <Button onClick={() => setCountryName('Morocco')}> Morocco</Button >
+        <Button onClick={() => setCountryName('Martinique')}> martinique</Button >
+      </div>
+      <Card {...{name,capital,region,population,flag}} />
 
-  
-
-      
     </div>
-  }
+  )
 }
 
 export default App
